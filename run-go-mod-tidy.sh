92adb61158ba5841bbfc4a097ce54b4de7455778
Naphat Sanguansin <naphat@prodvana.io>
2022-03-04 15:16:52 +0000

    support changing workdir for go-mod-tidy

--- run-go-mod-tidy.sh
+++ run-go-mod-tidy.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+while getopts "w:" opt; do
+  case $opt in
+    w)
+      cd $OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 if go mod tidy -v 2>&1 | grep -q 'updates to go.mod needed'; then
     exit 1
